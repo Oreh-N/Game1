b1c0d8d8038f48961b92addaa1cd089acad5e701
Oreh-N <nastia.orehowich@gmail.com>
2024-07-20 11:04:13 +0200

    Add main menu buttons functionality
    + Rename main game scene

---
+++ PlatformGame/Assets/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("The game is closed");
+         Application.Quit();
+    }
+}
