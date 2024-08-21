8046d929ef3ea6d1157cdc9848c8ccfa3f641a42
Oreh-N <nastia.orehowich@gmail.com>
2024-08-21 15:30:58 +0200

    Revert "Issues fixed"
    This reverts commit 818a2cdacaaa6ce9b0c665330d84ab6949a31fb9.

--- PlatformGame/Assets/Scripts/Player/PlayerController.cs
+++ PlatformGame/Assets/Scripts/Player/PlayerController.cs
@@ -69,7 +69,6 @@ public class PlayerController  : MonoBehaviour
             Jump();
             anim.SetTrigger("takeOff");
         }
-
         if (Input.GetButton("Horizontal"))
         {
             Move();
@@ -78,10 +77,8 @@ public class PlayerController  : MonoBehaviour
             else if (goingRight == false && Input.GetAxis("Horizontal") < 0)
                 Flip();
         }
-
         if (Input.GetKeyDown(KeyCode.E))
             CheckInteraction();
-
         if (Input.GetAxis("Horizontal") == 0)
             anim.SetBool("isRunning", false);
         else
@@ -91,7 +88,23 @@ public class PlayerController  : MonoBehaviour
     private void Move()
     {
         float dir = Input.GetAxis("Horizontal");
+
         rb.velocity = new Vector2(dir * speed, rb.velocity.y);
+        /*
+         * rb.velocity = new Vector2(dir * speed, rb.velocity.y);
+
+        if (dir != 0)
+        {
+            Vector3 scaler = transform.localScale;
+            scaler.x *= dir;
+            transform.localScale = scaler;
+        }
+        */
+        /*
+        transform.position += speed * dir * Time.deltaTime;
+        sprite.flipX = dir.x < 0.0f;
+        sword.flipY = dir.x < 0.0f;
+        */
     }
 
     private void Jump()
@@ -103,6 +116,11 @@ public class PlayerController  : MonoBehaviour
         Vector3 scaler = transform.localScale;
         scaler.x *= -1;
         transform.localScale = scaler;
+
+        if (Input.GetAxis("Horizontal") > 0)
+        { transform.eulerAngles = new Vector3(0, 180, 0); }
+        else if (Input.GetAxis("Horizontal") < 0)
+        { transform.eulerAngles = new Vector3(0, 0, 0); }
     }
 
     private bool IsGrounded()
