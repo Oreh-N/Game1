bea5d8e026d65dce54e3800f020969809b9a0f27
Oreh-N <nastia.orehowich@gmail.com>
2024-08-23 10:18:26 +0200

    Items drop (from bag) fixed

--- PlatformGame/Assets/Scripts/Inventory/Spawn.cs
+++ PlatformGame/Assets/Scripts/Inventory/Spawn.cs
@@ -12,7 +12,7 @@ public class Spawn : MonoBehaviour
 
     public void SpawnDroppedItem()
     {
-        Vector2 playerPos = new Vector2(player.position.x + 2, player.position.y - 1);
+        Vector2 playerPos = new Vector2(player.position.x + 1, player.position.y - 1);
         Instantiate(item, playerPos, Quaternion.identity);
     }
 }
